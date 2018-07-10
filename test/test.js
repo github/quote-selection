fe4e030b0e9b86924a4706fcdab6cb6275b18059
Mu-An ✌️ Chiou <muan@github.com>
2018-07-09 22:57:06 -0400

    Add test for markdown quote

--- test/test.js
+++ test/test.js
@@ -1,37 +1,45 @@
 /* global quoteSelection */
 
+function createSelection(el) {
+  const selection = window.getSelection()
+  const range = document.createRange()
+  range.selectNodeContents(el)
+  selection.removeAllRanges()
+  selection.addRange(range)
+  return selection
+}
+
 describe('quote-selection', function() {
   describe('with quotable selection', function() {
     beforeEach(function() {
       document.body.innerHTML = `
         <p id="not-quotable">Not quotable text</p>
-        <p id="quotable">Quotable text</p>
+        <p id="quotable">
+          Test <a href="#">Quotable</a> text, <strong>bold</strong>.</p>
         <textarea>Has text</textarea>
       `
       const el = document.querySelector('#quotable')
-      window.getSelection = function() {
-        return {
-          focusNode: el,
-          toString: () => el.textContent
-        }
-      }
+      const selection = createSelection(el)
+      window.getSelection = () => selection
     })
 
     afterEach(function() {
       document.body.innerHTML = ''
     })
 
-    it('textarea is updated', function(done) {
+    it('textarea is updated', function() {
       const container = document.querySelector('#quotable')
       const textarea = document.querySelector('textarea')
+      let eventCount = 0
 
       container.addEventListener('quote-selection', function() {
-        done()
+        eventCount++
       })
 
-      const quoted = quoteSelection.default(container, textarea)
-      assert(quoted)
-      assert.equal(textarea.value, 'Has text\n\n> Quotable text\n\n')
+      const quoted = quoteSelection(container, textarea)
+      assert.equal(quoted, 1)
+      assert.equal(textarea.value, 'Has text\n\n> Test [Quotable](#) text, **bold**.\n\n')
+      assert.equal(eventCount, 1)
     })
   })
 
@@ -43,12 +51,8 @@ describe('quote-selection', function() {
         <textarea>Has text</textarea>
       `
       const el = document.querySelector('#not-quotable')
-      window.getSelection = function() {
-        return {
-          focusNode: el,
-          toString: () => el.textContent
-        }
-      }
+      const selection = createSelection(el)
+      window.getSelection = () => selection
     })
 
     afterEach(function() {
@@ -58,7 +62,7 @@ describe('quote-selection', function() {
     it('textarea is not updated', function() {
       const container = document.querySelector('#quotable')
       const textarea = document.querySelector('textarea')
-      const quoted = quoteSelection.default(container, textarea)
+      const quoted = quoteSelection(container, textarea)
 
       assert(!quoted)
       assert.equal(textarea.value, 'Has text')
