d1c58a93287cec98df4dd571babded12a57987ab
Keith Cirkel <keithamus@users.noreply.github.com>
2021-11-01 09:32:25 +0000

    Use `AbortController` to simplify `uninstall`.
    This drastically reduces the amount of complex state we need to keep
    track of just to unbind event listeners.
    
    Co-authored-by: Kate Higa <khiga8@github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import {extractFragment, insertMarkdownSyntax} from './markdown'
 
 const containers: WeakMap<Element, Options> = new WeakMap()
-let installed = 0
+const controllers: WeakMap<Element, AbortController> = new WeakMap()
+
+let firstInstall = true
 
 type Options = {
   quoteMarkdown: boolean
@@ -10,8 +12,6 @@ type Options = {
 }
 
 export function install(container: Element, options?: Partial<Options>) {
-  const firstInstall = installed === 0
-  installed += containers.has(container) ? 0 : 1
   const config: Options = Object.assign(
     {
       quoteMarkdown: false,
@@ -21,11 +21,19 @@ export function install(container: Element, options?: Partial<Options>) {
     options
   )
   containers.set(container, config)
+  const controller = new AbortController()
+  controllers.set(container, controller)
   if (firstInstall) {
-    document.addEventListener('keydown', quoteSelection)
+    document.addEventListener('keydown', quoteSelection, {signal: controller.signal})
+    controller.signal.addEventListener('abort', () => {
+      firstInstall = true
+    })
+    firstInstall = false
   }
   if (config.copyMarkdown) {
-    ;(container as HTMLElement).addEventListener('copy', onCopy)
+    ;(container as HTMLElement).addEventListener('copy', onCopy, {
+      signal: controller.signal
+    })
   }
 }
 
@@ -33,13 +41,8 @@ export function uninstall(container: Element) {
   const config = containers.get(container)
   if (config == null) return
   containers.delete(container)
-  installed -= 1
-  if (installed === 0) {
-    document.removeEventListener('keydown', quoteSelection)
-  }
-  if (config.copyMarkdown) {
-    ;(container as HTMLElement).removeEventListener('copy', onCopy)
-  }
+  controllers.get(container)?.abort()
+  controllers.delete(container)
 }
 
 function onCopy(event: ClipboardEvent) {
