262f190dac815b9482a6b1b4dbbb6fe017964e52
Dusty Greif <dgreif@github.com>
2025-10-07 15:53:43 -0400

    Enable headless mode

--- vitest.config.js
+++ vitest.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     browser: {
       enabled: true,
       provider: 'playwright',
+      headless: true,
       instances: [
         {
           browser: 'chromium'
