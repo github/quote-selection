c51739c89f3a55e2dabdb587534469f2f3af334e
Dusty Greif <dgreif@github.com>
2025-10-07 15:53:32 -0400

    Install chromium in CI

--- vitest.config.js
+++ vitest.config.js
@@ -8,10 +8,7 @@ export default defineConfig({
       provider: 'playwright',
       instances: [
         {
-          browser: 'chromium',
-          launch: {
-            executablePath: '/usr/bin/chromium-browser'
-          }
+          browser: 'chromium'
         }
       ]
     }
